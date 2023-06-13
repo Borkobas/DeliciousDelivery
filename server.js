f9d3346d607b0d3279890f3fbb9bc421ff5b522d
Borkobas <boris.nemet@gmail.com>
2023-06-13 19:26:33 +0200

    installed express and nodemon, continuing with mongo-db connection

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Server is running!" + port);
+  }); 
+
+  const port = process.env.PORT || 8000;
+
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+
+  /*
+const Dish = require('./models/dishModel');
+const db = require("./db");
+
+
+
+
+const dishesRoute = require('./routes/dishesRoute');
+
+app.use('/api/dishes/', dishesRoute)
+ 
+app.get("/", (req, res) => {
+  res.send("Server is running!" + port);
+}); 
+
+*/
+
