f285a9ccc08302ddfbbf96ed1bf2976af265cb36
Borkobas <boris.nemet@gmail.com>
2023-06-13 22:38:25 +0200

    Connected to Mongo DB Atlas

---
+++ db.js
@@ -0,0 +1,13 @@
+
+const mongoose = require("mongoose");
+var mongoURL = 'mongodb+srv://noviuporabnik:uporabnik@cluster5.dlcfhvs.mongodb.net/project-dish'
+mongoose.connect(mongoURL , {useUnifiedTopology:true , useNewUrlParser:true})
+
+var db = mongoose.connection
+db.on('connected' , ()=>{
+console.log('Mongo DB Connection Successfull');
+})
+db.on ('error' , ()=>{
+console.log('Mongo DB Connection failed');
+})
+module.exports =mongoose
